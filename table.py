ce60e6f6986ff79aa8ca4798b53f2fb39c689a25
Sebastian Lyng Johansen <seb.l.j@hotmail.com>
2020-10-15 22:16:47 +0200

    minor improvements

--- table.py
+++ table.py
@@ -7,115 +7,72 @@ import sys
 
 class Main:
     def __init__(self):
-        self.scraper = Scraper()
         self.url = "https://www.skysports.com/"
         self.league = ""    
         self.leaguetitle = ""
         self.year = ""
- 
-    def setLeagueTitle(self):
 
-        if str(self.league) == 'premier-league':
-            self.leaguetitle = 'Premier League'
-        elif str(self.league) == 'la-liga':
-            self.leaguetitle = 'La Liga'
-        elif str(self.league) == 'bundesliga':
-            self.leaguetitle = 'Bundesliga'
-        elif str(self.league) == 'serie-a':
-            self.leaguetitle = 'Serie A'
-        elif str(self.league) == 'eredivisie':
-            self.leaguetitle = 'Eredivisie'
-        elif str(self.league) == 'ligue-1':
-            self.leaguetitle = 'Ligue 1'
-        elif str(self.league) == 'championship':
-            self.leaguetitle = 'Championship'
-        elif str(self.league) == 'champions-league':
-            self.leaguetitle = 'Champions League'
+    # Find the year the user wants to see the table for
+    def parseArgument(self):
 
-
-    def scrape(self):
-
+        # if user specifies year, check if writte for example '15' or '2015'
         if len(sys.argv) > 1:
             self.year = sys.argv
-            self.year.pop(0)
-            self.year = "20" + str((str(self.year)[2]) + str(self.year)[3])
-            secondyear = int(str(self.year)[2] + str(self.year)[3]) + 1
-            print(secondyear)
+            inputyear = self.year.pop(1)
+            if len(inputyear) > 2:
+                self.year = inputyear
+            else:
+                self.year = '20' + inputyear[0] + inputyear[1]
+ 
+    # Scrape the page and print football table
+    def scrape(self):
 
+        self.parseArgument()
 
         league = input("Hvilken liga vil du se tabell for?\n")
         self.league = self.determineLeague(league)
         self.url = self.url + self.league + "-table/" + self.year
 
-        page_html = self.scraper.scrapePage(self.url)
+        page_html = Scraper().scrapePage(self.url)
         page_soup = soup(page_html, "html.parser")
         
         tables = page_soup.findAll("table", {"class":{"standing-table__table"}})
+        key_info = page_soup.find("div", {"class":"standing-table__supplementary"})
 
-        if not len(tables):
+        if not tables:
             print("Table not found")
             return
 
-        self.setLeagueTitle()
-
         tabledirectory = {}
 
-        tablenum = 0
+        for idx, table in enumerate(tables):
 
-        for x in range(0, len(tables)):
-            tabledirectory["t{0}".format(x)]=PrettyTable()
-
-        # Key information on the bottom 
-        key_info = page_soup.find("div", {"class":"standing-table__supplementary"})
-
-        for table in tables:
-
-            t = tabledirectory["t" + str(tablenum)]
+            tabledirectory["t{0}".format(idx)]=PrettyTable()
+            t = tabledirectory["t" + str(idx)]
 
             t.field_names = ['#', 'Team', 'P', 'W', 'D', 'L', 'F', 'A', 'GD', 'Pts', 'Form[W,D,L]']
             tabletitle = table.findAll("caption", {"class":"standing-table__caption"})
             t.title = tabletitle[0].text.strip()
             team = table.findAll("td", {"class":"standing-table__cell standing-table__cell--name"})
-            teamlist = []
             rest = table.findAll("td", {"class":"standing-table__cell"})
             form = table.findAll("div", {"class":"standing-table__form"})
 
-
-            jump = 11
-            posindex = 0
-            playedindex = 2
-            winindex = 3
-            drawindex = 4
-            lossindex = 5
-            goalscoredindex = 6
-            goalsconcededindex = 7
-            gdindex = 8
-            pointsindex = 9
-
             for i in range(0, len(team)):
-                teamlist.append(team[i].get("data-long-name"))
-                teamname = teamlist[i]
                 
-                pos = rest[posindex].text
-                played = rest[playedindex].text
-                win = rest[winindex].text
-                draw = rest[drawindex].text
-                lose = rest[lossindex].text
-                goalscored = rest[goalscoredindex].text
-                goalsconceded = rest[goalsconcededindex].text
-                gd = rest[gdindex].text
-                points = rest[pointsindex].text
-                posindex += jump
-                playedindex += jump
-                winindex += jump
-                drawindex += jump
-                lossindex += jump
-                goalscoredindex += jump
+                teamname = team[i].get("data-long-name")
+                # calculate position, number of played games, number of wins etc... for each team
+                # Some magic numbers for parsing the html
+                # 11 is how much to move the index to get to the next team.
+                pos = rest[0 + (i * 11)].text
+                played = rest[2 + (i * 11)].text
+                win = rest[3 + (i * 11)].text
+                draw = rest[4 + (i * 11)].text
+                lose = rest[5 + (i * 11)].text
+                goalscored = rest[6 + (i * 11)].text
+                goalsconceded = rest[7 + (i * 11)].text
+                gd = rest[8 + (i * 11)].text
+                points = rest[9 + (i * 11)].text
 
-                goalsconcededindex += jump
-                gdindex +=  jump
-                pointsindex += jump
-                
                 # If the list is empty, there is no information on form. 
                 # This happens on table for previous years
                 try:
@@ -126,58 +83,57 @@ class Main:
                 t.add_row([pos, teamname, played, win, draw, lose, goalscored, goalsconceded, gd, points, newform])
 
             print(t)
-            tablenum += 1
 
-        # Print key info like champions league spots, relegation etc...
+        self.printKeyInfo(key_info)
+
+    # Set the different league based on the input from the user.
+    # Do this to make it possible to write the leagues several ways like "pl" or "premier league".
+    def determineLeague(self, league):
+
+        league = str(league).lower()
+
+        leaguedict = {
+            'premier league' : 'premier-league',
+            'pl' : 'premier-league',
+            'england' : 'premier-league',
+            'la liga' : 'la-liga',
+            'spain' : 'la-liga',
+            'spania' : 'la-liga',
+            'cl' : 'champions-league',
+            'champions league' : 'champions-league',
+            'serie a' : 'serie-a',
+            'italy' : 'serie-a',
+            'italia' : 'serie-a',
+            'ligue 1' : 'ligue-1',
+            'france' : 'ligue-1',
+            'frankrike' : 'ligue-1',
+            'league one' : 'league-1',
+            'leauge 1' : 'league-1',
+            'leauge two' : 'league-2',
+            'league 2' : 'league-2',
+            'nations league' : 'uefa-nations-league',
+            'uefa nations league' : 'uefa-nations-league',
+            'europa league' : 'europa-league',
+            'el' : 'europa-leauge',
+            'copa america' : 'copa-america',
+            'scottish premiership' : 'scottish-premier-league',
+            'scotland' : 'scottish-premier-league'
+        }
+
+        try:
+            return leaguedict[league]
+        except KeyError:
+            return league
+
+
+    # Print important info about the league
+    def printKeyInfo(self, key_info):
         for child in key_info.children:
             if child.string != None and child.string != "Key":
                 print(child.string)
             if child.string == "Key":
                 print("\n")
 
-
-    # Set the different league based on the input from the user.
-    # Do this to make it possible to write the leagues several ways like "pl" or "premier league".
-    def determineLeague(self, league):
-        premier_league = 'premier league'
-        pl = 'pl'
-        la_liga = 'la liga'
-        bundesliga = 'bundesliga'
-        ligue_1 = 'ligue 1'
-        championship = 'championship'
-        serie_a = 'serie a'
-        eredivisie = 'eredivisie'
-        cl = 'cl'
-        champions_league = 'champions league'
-        league_one = 'league one'
-        league_two = 'league two'
-
-        if str(league).lower() == premier_league.lower():
-            return 'premier-league'
-        elif str(league).lower() == pl.lower():
-            return 'premier-league'
-        elif str(league).lower() == la_liga.lower():
-            return 'la-liga'
-        elif str(league).lower() == bundesliga.lower():
-            return 'bundesliga'
-        elif str(league).lower() == ligue_1.lower():
-            return 'ligue-1'
-        elif str(league).lower() == championship.lower():
-            return 'championship'
-        elif str(league).lower() == serie_a.lower():
-            return 'serie-a'
-        elif str(league).lower() == eredivisie.lower():
-            return 'eredivisie'
-        elif str(league).lower() == cl.lower():
-            return 'champions-league'
-        elif str(league).lower() == champions_league.lower():
-            return 'champions-league'
-        elif str(league).lower() == league_one.lower():
-            return 'league-1'
-        elif str(league).lower() == league_two.lower():
-            return 'league-two'
-
-        return league
 
     # Returns a list of the form for the previous matches [Win, draw, loss]
     def checkForm(self, form, i):
